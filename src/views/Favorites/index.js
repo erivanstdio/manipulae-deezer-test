8f19130741aafa8ac161f8f75535f12d1ba2e9ef
erivanstdio <erivanstdio@gmail.com>
2021-08-24 13:01:18 -0300

    using TrackList component to render favorited tracks

--- src/views/Favorites/index.js
+++ src/views/Favorites/index.js
@@ -1,7 +1,20 @@
+import { useSelector } from 'react-redux';
+
+import TracksList from '../../components/TracksList';
 import { Container } from './styles';
 
 const Favorites = () => {
-	return <Container></Container>;
+	const favorites = useSelector((state) => state?.tracks?.favorites);
+
+	if (!favorites?.length) {
+		return <span>Sem favoritos</span>;
+	}
+
+	return (
+		<Container>
+			<TracksList getFromFavorites />
+		</Container>
+	);
 };
 
 export default Favorites;
